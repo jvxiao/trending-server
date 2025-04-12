762532c28ce6b3c60e2e41798e6bfc777dd1f79f
jvxiao <2805431021@qq.com>
2025-04-12 18:47:28 +0800

    feat: add zhihu model

---
+++ models/trendings/zhihuModel.js
@@ -0,0 +1,37 @@
+const axios = require('axios')
+
+class ZhihuModel {
+  ZHIHU_TRENDING_URL = 'https://www.zhihu.com/api/v3/feed/topstory/hot-lists/total';
+  
+  getTrending(param = { limit: 50 }) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const res = await axios.get(this.ZHIHU_TRENDING_URL, param)
+        if(res.status === 200) {
+          resolve(this.formatData(res.data.data))
+        }
+      } catch(err) {
+        reject(err)
+        // throw err
+      }
+    })
+  }
+
+  formatData(data) {
+    if (!data || !data.length ) [];
+
+    return data.map(item => {
+      return {
+        title: item?.target?.title || '',
+        url: (item?.target?.url || '').replace('https://api.zhihu.com/questions', 'https://www.zhihu.com/question'),
+        excerpt: item?.target?.excerpt
+      }
+    })
+  }
+}
+
+module.exports = new ZhihuModel()
+// const zhihu = new ZhihuModel()
+// zhihu.getTrending().then(res => {
+//   console.log(res)
+// })
