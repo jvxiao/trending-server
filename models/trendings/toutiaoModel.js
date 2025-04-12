61dbd8b6f04dae19efae26770c130e00860c3591
jvxiao <2805431021@qq.com>
2025-04-12 18:47:46 +0800

    feat:  add toutiao model

---
+++ models/trendings/toutiaoModel.js
@@ -0,0 +1,39 @@
+const axios = require('axios')
+
+class ToutiaoModel {
+  TOUTIAO_TRENDING_URL = 'https://i-lq.snssdk.com/api/feed/hotboard_online/v1/?category=hotboard_online&count=50';
+  
+  getTrending(param = { 
+    count: 50,
+    category: 'hotboard_online'
+  }) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const res = await axios.get(this.TOUTIAO_TRENDING_URL, param)
+        if(res.status === 200) {
+          resolve(this.formatData(res.data.data));
+        }
+      } catch(err) {
+        reject(err)
+      }
+    })
+  }
+
+  formatData(data) {
+    if (!data || !data.length ) [];
+    data = data.map(o => JSON.parse(o.content))
+    return data.map(item => {
+      return {
+        title: item?.raw_data.title || '',
+        url: 'https://so.toutiao.com/search?keyword='+ item?.raw_data.title,
+        excerpt: ''
+      }
+    })
+  }
+}
+
+module.exports = new ToutiaoModel()
+// const zhihu = new ToutiaoModel()
+// zhihu.getTrending().then(res => {
+//   console.log(res)
+// })
